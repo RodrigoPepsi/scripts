a59143c69ca0dbe7b2c71fe2ea7ec178ff80b1ef
RodrigoPepsi <36858581+RodrigoPepsi@users.noreply.github.com>
2019-03-27 16:06:59 +0200

    Update badguy.lua

--- badguy.lua
+++ badguy.lua
@@ -8,8 +8,10 @@ badguy.optionSliderAutoLaugh = Menu.AddOptionSlider({"Utility", "Bad Guy"}, "Lau
 badguy.optionToxicFlame = Menu.AddOptionBool({"Utility", "Bad Guy"}, "Toxic Flame", false)
 badguy.optionLanguage = Menu.AddOptionCombo({"Utility","Bad Guy"}, "Language", {"Русский", "English"}, 0)
 badguy.phrase = {
-	"vk.com/cheats_dota",
-	"uc.zone",
+	"Уже можешь вставать АФК,лол",
+	"Соло",
+	"В могилу к матери",
+	"Додик,ахахах",
 	"Что-то ты слишком изи",
 	"Как два пальца",
 	"Слишком легко для меня"
